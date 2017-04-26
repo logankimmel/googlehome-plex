6b727a1c597ed490195b42a1964d3647a4582e6f
logankimmel <lkimmel@vmware.com>
2017-04-26 12:05:01 -0500

    Fix formatting

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
 process.env.DEBUG = 'actions-on-google:*';
 
 const ActionsSdkAssistant = require('actions-on-google').ActionsSdkAssistant;
-const PlexAPI = require("plex-api");
+const PlexAPI = require('plex-api');
 
 exports.plexInfo = (req, res) => {
   const assistant = new ActionsSdkAssistant({request: req, response: res});
-  var fs = require('fs');
-  var parseJson = require('parse-json');
+  const fs = require('fs');
+  const parseJson = require('parse-json');
   var opts = parseJson(fs.readFileSync('creds.json'));
 
   const client = new PlexAPI(opts);
@@ -33,63 +33,63 @@ exports.plexInfo = (req, res) => {
       assistant.tell('Goodbye!');
     } else {
       switch(raw) {
-        case 'name':
 
-          client.query("/").then(function (result) {
+        case 'name':
+          client.query('/').then(function (result) {
             var mc =  result.MediaContainer;
-            inputString = "<speak> Your Plex Server name is " + mc.friendlyName + "</speak>";
+            inputString = '<speak> Your Plex Server name is ' + mc.friendlyName + '</speak>';
             let inputPrompt = assistant.buildInputPrompt(true, inputString,
                 ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
             assistant.ask(inputPrompt);
           }, function (err) {
-            console.error("Could not connect to server", err);
+            console.error('Could not connect to server', err);
           });
           break;
 
         case 'recently added':
-          client.query("/library/recentlyAdded").then(function (result) {
+          client.query('/library/recentlyAdded').then(function (result) {
             var md = result.MediaContainer.Metadata;
-            inputString = "<speak> Most recent plex items are"
+            inputString = '<speak> Most recent plex items are'
             console.log("MD: %s", md)
             for(var i in md) {
               if (i == 4) {
                 break;
               }
               var item = md[i];
-              if (item.type == "season") {
-                var lineString = item.parentTitle + ", " + item.title + ", Episode " + item.leafCount;
+              if (item.type == 'season') {
+                var lineString = item.parentTitle + ', ' + item.title + ', Episode ' + item.leafCount;
               } else {
-                var lineString = "The movie, " + item.title;
+                var lineString = 'The movie, ' + item.title;
               }
-              inputString += ". " + lineString;
+              inputString += '. ' + lineString;
             }
-            inputString += ". </speak>";
+            inputString += '. </speak>';
             let inputPrompt = assistant.buildInputPrompt(true, inputString,
                 ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
             assistant.ask(inputPrompt);
           }, function (err) {
-            console.error("Could not connect to server", err);
+            console.error('Could not connect to server', err);
           });
           break;
 
         case 'on deck':
-          client.query("/library/onDeck").then(function (result) {
+          client.query('/library/onDeck').then(function (result) {
             inputString = '<speak> The items on deck are, <break time="1"/> '
             var od = result.MediaContainer.Metadata;
             for (var o in od) {
               inputString += od[o].title + ', <break time"1"/> ';
             }
-            inputString += ". </speak>";
+            inputString += '. </speak>';
             let inputPrompt = assistant.buildInputPrompt(true, inputString,
               ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
             assistant.ask(inputPrompt);
-        }, function (err) {
-          console.error("Could not connect to server", err);
-        });
-        break;
+          }, function (err) {
+            console.error('Could not connect to server', err);
+          });
+          break;
 
         case 'random movie':
-          client.query("/library/sections/1/all").then(function (res) {
+          client.query('/library/sections/1/all').then(function (res) {
             var totalMovies = res.MediaContainer.size;
             console.log("Total movies: %s", totalMovies)
             var random = Math.floor(Math.random() * totalMovies) + 0;
@@ -99,18 +99,18 @@ exports.plexInfo = (req, res) => {
                 ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
             assistant.ask(inputPrompt);
           }, function (err) {
-            console.error("Error calling to server", err);
+            console.error('Error calling to server', err);
           });
           break;
 
         case 'refresh movies':
-        client.perform("/library/sections/1/refresh").then(function (res) {
-          let inputPrompt = assistant.buildInputPrompt(true, "Successfully started refresh of movie library.",
-            ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
-          assistant.ask(inputPrompt);
-        }, function (err) {
-          console.error("Error refreshing movies", err);
-        });
+          client.perform('/library/sections/1/refresh').then(function (res) {
+            let inputPrompt = assistant.buildInputPrompt(true, 'Successfully started refresh of movie library.',
+              ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
+            assistant.ask(inputPrompt);
+          }, function (err) {
+            console.error('Error refreshing movies', err);
+          });
         break;
 
         default:
@@ -120,13 +120,6 @@ exports.plexInfo = (req, res) => {
           assistant.ask(inputPrompt);
       }
     }
-
-  }
-
-  function query(api, options) {
-    return api.postQuery(options).then(function(response) {
-        return response;
-    })
   }
 
   let actionMap = new Map();
