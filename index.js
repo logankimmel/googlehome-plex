01d941a91df0803df4cc4d42b5b8b3b65c863efe
logankimmel <lkimmel@vmware.com>
2017-04-28 10:14:31 -0500

    refactor

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ process.env.DEBUG = 'actions-on-google:*';
 
 const ActionsSdkAssistant = require('actions-on-google').ActionsSdkAssistant;
 const PlexAPI = require('plex-api');
+const parseJson = require('parse-json');
 
 exports.plexInfo = (req, res) => {
-  const assistant = new ActionsSdkAssistant({request: req, response: res});
-  const fs = require('fs');
-  const parseJson = require('parse-json');
+  var assistant = new ActionsSdkAssistant({request: req, response: res});
+  var fs = require('fs');
   var opts = parseJson(fs.readFileSync('creds.json'));
-
-  const client = new PlexAPI(opts);
+  var client = new PlexAPI(opts);
+  var idk = ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']
 
   function mainIntent (assistant) {
     console.log('mainIntent');
@@ -35,91 +35,106 @@ exports.plexInfo = (req, res) => {
       switch(raw) {
 
         case 'name':
-          client.query('/').then(function (result) {
-            var mc =  result.MediaContainer;
-            inputString = '<speak> Your Plex Server name is ' + mc.friendlyName + '</speak>';
-            let inputPrompt = assistant.buildInputPrompt(true, inputString,
-                ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
-            assistant.ask(inputPrompt);
-          }, function (err) {
-            console.error('Could not connect to server', err);
-          });
+          name();
           break;
 
         case 'recently added':
-          client.query('/library/recentlyAdded').then(function (result) {
-            var md = result.MediaContainer.Metadata;
-            inputString = '<speak> Most recent plex items are'
-            console.log("MD: %s", md)
-            for(var i in md) {
-              if (i == 4) {
-                break;
-              }
-              var item = md[i];
-              if (item.type == 'season') {
-                var lineString = item.parentTitle + ', ' + item.title + ', Episode ' + item.leafCount;
-              } else {
-                var lineString = 'The movie, ' + item.title;
-              }
-              inputString += '. ' + lineString;
-            }
-            inputString += '. </speak>';
-            let inputPrompt = assistant.buildInputPrompt(true, inputString,
-                ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
-            assistant.ask(inputPrompt);
-          }, function (err) {
-            console.error('Could not connect to server', err);
-          });
+          recentlyAdded();
           break;
 
         case 'on deck':
-          client.query('/library/onDeck').then(function (result) {
-            inputString = '<speak> The items on deck are, <break time="1"/> '
-            var od = result.MediaContainer.Metadata;
-            for (var o in od) {
-              inputString += od[o].title + ', <break time"1"/> ';
-            }
-            inputString += '. </speak>';
-            let inputPrompt = assistant.buildInputPrompt(true, inputString,
-              ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
-            assistant.ask(inputPrompt);
-          }, function (err) {
-            console.error('Could not connect to server', err);
-          });
+          onDeck();
           break;
 
         case 'random movie':
-          client.query('/library/sections/1/all').then(function (res) {
-            var totalMovies = res.MediaContainer.size;
-            console.log("Total movies: %s", totalMovies)
-            var random = Math.floor(Math.random() * totalMovies) + 0;
-            var movie = res.MediaContainer.Metadata[random];
-            var inputString = 'Your random movie is <break time="1"/> ' + movie.title;
-            let inputPrompt = assistant.buildInputPrompt(true, inputString,
-                ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
-            assistant.ask(inputPrompt);
-          }, function (err) {
-            console.error('Error calling to server', err);
-          });
+          randomMovie();
           break;
 
         case 'refresh movies':
-          client.perform('/library/sections/1/refresh').then(function (res) {
-            let inputPrompt = assistant.buildInputPrompt(true, 'Successfully started refresh of movie library.',
-              ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
-            assistant.ask(inputPrompt);
-          }, function (err) {
-            console.error('Error refreshing movies', err);
-          });
+          refreshMovies();
         break;
 
         default:
           var inputString = 'I\'m sorry, I don\'t know that information';
-          let inputPrompt = assistant.buildInputPrompt(true, inputString,
-              ['I didn\'t hear that', 'If you\'re still there, what would you like to know?']);
+          let inputPrompt = assistant.buildInputPrompt(true, inputString, idk);
           assistant.ask(inputPrompt);
       }
     }
+  }
+
+  function name() {
+    client.query('/').then(function (result) {
+      var mc =  result.MediaContainer;
+      inputString = '<speak> Your Plex Server name is ' + mc.friendlyName + '</speak>';
+      let inputPrompt = assistant.buildInputPrompt(true, inputString, idk);
+      assistant.ask(inputPrompt);
+    }, function (err) {
+      console.error('Could not connect to server', err);
+    });
+  }
+
+  function recentlyAdded() {
+    client.query('/library/recentlyAdded').then(function (result) {
+      var md = result.MediaContainer.Metadata;
+      inputString = '<speak> Most recent plex items are'
+      console.log("MD: %s", md)
+      for(var i in md) {
+        if (i == 4) {
+          break;
+        }
+        var item = md[i];
+        if (item.type == 'season') {
+          var lineString = item.parentTitle + ', ' + item.title + ', Episode ' + item.leafCount;
+        } else {
+          var lineString = 'The movie, ' + item.title;
+        }
+        inputString += '. ' + lineString;
+      }
+      inputString += '. </speak>';
+      let inputPrompt = assistant.buildInputPrompt(true, inputString, idk);
+      assistant.ask(inputPrompt);
+    }, function (err) {
+      console.error('Could not connect to server', err);
+    });
+  }
+
+  function onDeck() {
+    client.query('/library/onDeck').then(function (result) {
+      inputString = '<speak> The items on deck are, <break time="1"/> '
+      var od = result.MediaContainer.Metadata;
+      for (var o in od) {
+        inputString += od[o].title + ', <break time"1"/> ';
+      }
+      inputString += '. </speak>';
+      let inputPrompt = assistant.buildInputPrompt(true, inputString, idk);
+      assistant.ask(inputPrompt);
+    }, function (err) {
+      console.error('Could not connect to server', err);
+    });
+  }
+
+  function randomMovie() {
+    client.query('/library/sections/1/all').then(function (res) {
+      var totalMovies = res.MediaContainer.size;
+      console.log("Total movies: %s", totalMovies)
+      var random = Math.floor(Math.random() * totalMovies) + 0;
+      var movie = res.MediaContainer.Metadata[random];
+      var inputString = 'Your random movie is <break time="1"/> ' + movie.title;
+      let inputPrompt = assistant.buildInputPrompt(true, inputString, idk);
+      assistant.ask(inputPrompt);
+    }, function (err) {
+      console.error('Error calling to server', err);
+    });
+  }
+
+  function refreshMovies() {
+    client.perform('/library/sections/1/refresh').then(function (res) {
+      let inputPrompt = assistant.buildInputPrompt(true, 'Successfully started refresh of movie library.',
+        idk);
+      assistant.ask(inputPrompt);
+    }, function (err) {
+      console.error('Error refreshing movies', err);
+    });
   }
 
   let actionMap = new Map();
